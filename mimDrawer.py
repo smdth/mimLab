2492f7353f6bc63cef474c0519f647ad82b56824
smdth <thomas.3.schmidt@uni-konstanz.de>
2015-11-25 16:33:52 +0000

    looks like a face now

--- mimDrawer.py
+++ mimDrawer.py
@@ -57,12 +57,10 @@ def drawPath(path, precision=10):
     return p
 
 ### DRAW HEAD ###
-headX, headY = drawLine([0.2,2], [0,2], 10)
-headX, headY = genericAppend((headX, headY), (shift(quartCircle(10)[0], 0.2),
-                                              shift(quartCircle(10)[1], 1)))
+headX, headY = quartCircle(100)[0], shift(quartCircle(100)[1], 0.8)
 headX,headY = genericAppend((headX, headY),
-                            (drawLine([1.2,1], [1.2,0], 10)[0][::-1],
-                             drawLine([1.2,1], [1.2,0], 10)[1][::-1]))
+                            (drawLine([1,0.8], [1,0], 100)[0][::-1],
+                             drawLine([1,0.8], [1,0], 100)[1][::-1]))
 headX,headY = mirrorVert(*mirrorHor(headX, headY))
 
 head = headX, headY
@@ -70,25 +68,65 @@ head = headX, headY
 ### DRAW LEFT EYE ###
 
 leftEyeX, leftEyeY = mirrorVert(*mirrorHor(*quartCircle(100)))
-leftEyeX = scale(leftEyeX, 0.25)
-leftEyeY = scale(leftEyeY, 0.25)
+leftEyeX = scale(leftEyeX, 0.3)
+leftEyeY = scale(leftEyeY, 0.3)
 leftEyeX = shift(leftEyeX, -0.35)
-leftEyeY = shift(leftEyeY, 0.25)
+leftEyeY = shift(leftEyeY, 0.55)
+
+leftEye = leftEyeX, leftEyeY
 
 ### DRAW RIGHT EYE ###
 
-rightEyeX, rightEyeY = mirrorVert(leftEyeX, leftEyeY)
+rightEyeX, rightEyeY = mirrorVert(*mirrorHor(*quartCircle(100)))
+rightEyeX = scale(rightEyeX, 0.3)
+rightEyeY = scale(rightEyeY, 0.3)
+rightEyeX = shift(rightEyeX, 0.4)
+rightEyeY = shift(rightEyeY, 0.55)
+
+rightEye = rightEyeX, rightEyeY
+
+### DRAW HAIR ###
 
 hair = [[-1,0], [-1.5,0.5], [-1,0.5], [-1.4,1], [-0.75,1], [-1.2,1.7],
         [-0.47,1.4], ]
 #lineX, lineY = drawPath(hair, 10)
+
+
+### DRAW MOUTH ###
+
+mouth = quartCircle(100)
+mouth = scale(mouth[0], 1.2), scale(mouth[1], 0.2)
+mouth = shift(mouth[0], -0.5), shift(mouth[1], -0.8)
+
+### DRAW DETAILS ###
+
+## EYES ##
+detail1 = quartCircle(100)[0], tuple(map(lambda n: -n, quartCircle(100)[1]))
+detail1 = scale(detail1[0], 0.3), scale(detail1[1], 0.2)
+detail1 = shift(detail1[0], -0.38), shift(detail1[1], 0.3)
+
+detail2 = quartCircle(100)[0][::-1], tuple(map(lambda n: -n,
+                                               quartCircle(100)[1]))
+detail2 = scale(detail2[0], 0.3), scale(detail2[1], 0.2)
+detail2 = shift(detail2[0], 0.16), shift(detail2[1], 0.3)
+
+detail3 = drawPath([[-0.3, 0.55], [-0.3, 0.45], [-0.25, 0.5], [-0.35, 0.5]],
+                   10)
+detail4 = drawPath([[0.35, 0.6], [0.35, 0.5], [0.3, 0.55], [0.4, 0.55]], 10)
+
 
 #fullX = headX + leftEyeX + rightEyeX + lineX
 #fullY = headY + leftEyeY + rightEyeY + lineY
 
 #plot.scatter(fullX,fullY)
 plot.plot(*head)
-plot.plot(leftEyeX, leftEyeY)
+plot.plot(*leftEye)
+plot.plot(*rightEye)
+plot.plot(*mouth)
+plot.plot(*detail1)
+plot.plot(*detail2)
+plot.plot(*detail3)
+plot.plot(*detail4)
 plot.axis([-4,4,-4,4])
 plot.axes().set_aspect('equal', 'datalim')
 #plot.savefig('plot.png')
