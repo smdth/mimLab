da57351928a5b6c394c7119ad74d7577a3ccb2e3
smdth <thomas.3.schmidt@uni-konstanz.de>
2015-11-25 14:37:20 +0000

    head done

--- mimDrawer.py
+++ mimDrawer.py
@@ -17,15 +17,15 @@ def quartCircle(precision):
     return x,y
 
 def mirrorHor(x,y):
-    x = x + x
-    yTemp = [ -i for i in y ]
+    x = x + x[::-1]
+    yTemp = [ -i for i in reversed(y) ]
     y = y + yTemp
     return x,y
 
 def mirrorVert(x,y):
-    xTemp = [ -i for i in x ]
+    xTemp = [ -i for i in reversed(x) ]
     x = x + xTemp
-    y = y + y
+    y = y + y[::-1]
     return x,y
 
 def shift(x, c):
@@ -41,8 +41,33 @@ def drawLine(p1, p2, precision):
     y = [ p1[1] + (1 - (float(t)/float(precision))) * (p2[1] - p1[1]) for t in range(precision + 1)]
     return x,y
 
-headX,headY = mirrorVert(*mirrorHor(*quartCircle(100)))
-headY = scale(headY, 1.5)
+def lineAppend(l, p1, p2, precision):
+    l = tuple(map(operator.add, l, drawLine(p1, p2, precision)))
+    return l
+
+def genericAppend(p1, p2):
+    p1 = tuple(map(operator.add, p1, p2))
+    return p1
+
+def drawPath(path, precision=10):
+    p = [],[]
+    for i in range(len(path)-1):
+        p = lineAppend(p, path[i], path[i+1], precision)
+
+    return p
+
+### DRAW HEAD ###
+headX, headY = drawLine([0.2,2], [0,2], 10)
+headX, headY = genericAppend((headX, headY), (shift(quartCircle(10)[0], 0.2),
+                                              shift(quartCircle(10)[1], 1)))
+headX,headY = genericAppend((headX, headY),
+                            (drawLine([1.2,1], [1.2,0], 10)[0][::-1],
+                             drawLine([1.2,1], [1.2,0], 10)[1][::-1]))
+headX,headY = mirrorVert(*mirrorHor(headX, headY))
+
+head = headX, headY
+
+### DRAW LEFT EYE ###
 
 leftEyeX, leftEyeY = mirrorVert(*mirrorHor(*quartCircle(100)))
 leftEyeX = scale(leftEyeX, 0.25)
@@ -50,22 +75,21 @@ leftEyeY = scale(leftEyeY, 0.25)
 leftEyeX = shift(leftEyeX, -0.35)
 leftEyeY = shift(leftEyeY, 0.25)
 
+### DRAW RIGHT EYE ###
+
 rightEyeX, rightEyeY = mirrorVert(leftEyeX, leftEyeY)
 
-lineX, lineY = tuple(map(operator.add, drawLine([-1,0], [-1.5,0.5], 10),
-                        drawLine([-1.5,0.5], [-1,0.5], 10)))
-lineX, lineY = tuple(map(operator.add, (lineX, lineY),
-                         drawLine([-1,0.5], [-1.4,1], 10)))
-lineX, lineY = tuple(map(operator.add, (lineX, lineY),
-                        drawLine([-1.5,0.5], [-1,0.5], 10)))
-lineX, lineY = tuple(map(operator.add, (lineX, lineY),
-                        drawLine([-1.5,0.5], [-1,0.5], 10)))
+hair = [[-1,0], [-1.5,0.5], [-1,0.5], [-1.4,1], [-0.75,1], [-1.2,1.7],
+        [-0.47,1.4], ]
+#lineX, lineY = drawPath(hair, 10)
 
-fullX = headX + leftEyeX + rightEyeX + lineX
-fullY = headY + leftEyeY + rightEyeY + lineY
+#fullX = headX + leftEyeX + rightEyeX + lineX
+#fullY = headY + leftEyeY + rightEyeY + lineY
 
-plot.scatter(fullX,fullY)
-plot.axis([-2,2,-2,2])
+#plot.scatter(fullX,fullY)
+plot.plot(*head)
+plot.plot(leftEyeX, leftEyeY)
+plot.axis([-4,4,-4,4])
 plot.axes().set_aspect('equal', 'datalim')
 #plot.savefig('plot.png')
 plot.show()
